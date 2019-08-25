0cf8be53e6f85502f455d91a018820dcd930fc11
unknown <keegood8@gmail.com>
2019-08-25 10:41:07 -0700

    replace clouds.js with css animation versions of nav stripes. Page content hidden. Setup for load bar creation.

--- app/assets/javascripts/cloud.js
+++ app/assets/javascripts/cloud.js
@@ -1,23 +1,40 @@
-class Cloud {
-  constructor(elem, startX, startY, targX, targY){
-    this.pos = new p5.Vector(startX, startY);
-    this.vel = new p5.Vector(0,0);
-    this.acc = new p5.Vector(0,0);
-    this.targ = new p5.Vector(targX, targY);
-    this.elem = elem;
-  }
-
-  applyForce(force){
-    this.acc.add(force);
-  }
-
-  update(){
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  display(){
-
-  }
-}
+// class Cloud {
+//   constructor(elem, startX, startY, targX, targY){
+//     this.pos = new p5.Vector(startX, startY);
+//     this.vel = new p5.Vector(0,0);
+//     this.acc = new p5.Vector(0,0);
+//     this.targ = new p5.Vector(targX, targY);
+//     this.elem = elem;
+//     this.maxSpeed = 5;
+//     this.maxForce = 0.1;
+//   }
+//
+//   applyForce(force){
+//     this.acc.add(force);
+//   }
+//
+//   update(){
+//     this.vel.add(this.acc);
+//     this.vel.limit(this.maxSpeed);
+//     this.pos.add(this.vel);
+//     this.acc.mult(0);
+//   }
+//
+//   shift(){
+//     let targ = new p5.Vector(random(this.pos.x - 200, this.pos.x + 20), random(this.pos.y - 20, this.pos.y + 20));
+//
+//     console.log(targ);
+//
+//     this.seek(targ);
+//   }
+//
+//   seek(target){
+//     let desired = target.sub(this.pos);
+//     desired.normalize();
+//     desired.mult(this.maxForce);
+//     let steer = desired.sub(this.vel);
+//     steer.limit(this.maxForce);
+//
+//     this.applyForce(steer);
+//   }
+// }
--- app/assets/javascripts/clouds.js
+++ app/assets/javascripts/clouds.js
@@ -1,19 +1,28 @@
-let clouds = [];
-
-function setup(){
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('background-holder');
-
-  let cloudNum = $('.cloud').length;
-
-  for(let i=0; i<cloudNum; i++){
-     let c = select('cloud'+i, 'cloud-holder'); //dom cloud image
-     let cloud = new Cloud(c, 0,0,0,0); //cloud class for moving clouds
-     clouds.push(cloud);
-  }
-}
-
-function draw(){
-
-  background(0);
-}
+// let clouds = [];
+// let cloudNum;
+//
+// function setup(){
+//   let canvas = createCanvas(windowWidth, windowHeight);
+//   canvas.parent('background-holder');
+//
+//   cloudNum = $('.cloud').length;
+//
+//   for(let i=0; i<cloudNum; i++){
+//      let c = select('cloud'+i, 'cloud-holder'); //dom cloud image
+//      let cloud = new Cloud(c, 0,0,0,0); //cloud class for moving clouds
+//      clouds.push(cloud);
+//   }
+// }
+//
+// function draw(){
+//
+//   let x = random(1);
+//   if(x<.005){
+//     console.log("called");
+//     for(let i=0; i<clouds.length-1; i++){
+//       clouds[i].shift();
+//       clouds[i].update();
+//     }
+//   }
+//   background(0);
+// }
